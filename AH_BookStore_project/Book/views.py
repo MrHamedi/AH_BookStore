624337419751dcbcf2c2ccb654aec4b5c57bea82
MrHamedi <ahmadihamed167@gmail.com>
2020-11-15 00:32:17 +0330

    adding & homepage&book_detail_view

--- AH_BookStore_project/Book/views.py
+++ AH_BookStore_project/Book/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.views.generic.list import ListView 
+from Book.models import Book
+from django.shortcuts import render,get_object_or_404 
 
-# Create your views here.
+
+class BookListView(ListView):
+	model=Book 
+	paginate_by=3
+	template_name="book/homepage.html"
+	context_object_name="books"
+
+
+def book_detail_view(request,book_slug):
+	book=get_object_or_404(Book,slug=book_slug)
+	return(render(request,"book/book_detail.html",{"book":book}))
